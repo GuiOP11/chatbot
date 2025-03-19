c558c22754b71e3eb569149355c85a6080019a36
GuiOP11 <143406425+GuiOP11@users.noreply.github.com>
2025-03-19 18:46:52 +0000

    2

---
+++ javascript.js
@@ -0,0 +1,4 @@
+function iniciarChat() {
+    alert("Chat iniciado! Em breve, você poderá interagir com o chatbot.");
+    // Aqui você pode redirecionar para a página do chatbot ou abrir um modal.
+}
